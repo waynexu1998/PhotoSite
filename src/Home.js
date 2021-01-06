3a93932050cb224e5e2f2ae721c03c021901d002
waynexu <jiangyong.xu1998@gmail.com>
2021-01-06 00:10:52 -0800

    initial changes

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,17 @@
 import './Home.css'
-import './raster/raster2.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1 className="mainHeading">W   X</h1>
+        <div className="main_Div">
+            <div className="left_Div">
+                <div className="title_Div">
+                    <h1 className="main_Heading">
+                        W<span className="title_X">X</span>
+                    </h1>
+                    <div className="rect"></div>
+                </div>
+            </div>
+            <div className="right_Div"></div>
         </div >
     )
 }
